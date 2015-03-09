6b201bd385594e4d69308d180364e8a858f43b0a
mrxz <n.t.huisman@gmail.students.uu.nl>
2015-03-09 11:00:41 +0100

    Implemented some missing functions in the BigEndianBinaryReader

--- Motion lie detection/BigEndianBinaryReader.cs
+++ Motion lie detection/BigEndianBinaryReader.cs
@@ -3,13 +3,18 @@ using System.IO;
 
 namespace Motion_lie_detection
 {
+	/**
+	 * A BigEndian version of the standard C# BinaryReader.
+	 * The default BinaryReader assumes the input to be little endian
+	 * when reading multi-byte types with no means of altering or specifying it.
+	 * This BigEndianBinaryReader first reads the number of bytes required for the type,
+	 * reverses them and returns the value.
+	 */
 	public class BigEndianBinaryReader : BinaryReader
 	{
 
 		public BigEndianBinaryReader(Stream input) : base(input)
-		{
-
-		}
+		{ }
 
 		public override int ReadInt32 ()
 		{
@@ -39,27 +44,37 @@ namespace Motion_lie_detection
 
 		public override double ReadDouble ()
 		{
-			throw new NotSupportedException ();
+			byte[] bytes = ReadBytes (8);
+			Array.Reverse (bytes);
+			return BitConverter.ToDouble (bytes, 0);
 		}
 
 		public override short ReadInt16 ()
 		{
-			throw new NotSupportedException ();
+			byte[] bytes = ReadBytes (8);
+			Array.Reverse (bytes);
+			return BitConverter.ToInt16 (bytes, 0);
 		}
 
 		public override long ReadInt64 ()
 		{
-			throw new NotSupportedException ();
+			byte[] bytes = ReadBytes (8);
+			Array.Reverse (bytes);
+			return BitConverter.ToInt64 (bytes, 0);
 		}
 
 		public override ushort ReadUInt16 ()
 		{
-			throw new NotSupportedException ();
+			byte[] bytes = ReadBytes (2);
+			Array.Reverse (bytes);
+			return BitConverter.ToUInt16 (bytes, 0);
 		}
 
 		public override ulong ReadUInt64 ()
 		{
-			throw new NotSupportedException ();
+			byte[] bytes = ReadBytes (8);
+			Array.Reverse (bytes);
+			return BitConverter.ToUInt64 (bytes, 0);
 		}
 
 	}
